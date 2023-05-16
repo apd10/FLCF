ab7f6dc16f1cff4f4b0216bb4f709ecfe0583aa3
Aditya Desai <apd10@yogi.cs.rice.edu>
2023-05-15 20:39:12 -0500

    skewed allocation change

--- fair.py
+++ fair.py
@@ -48,12 +48,13 @@ def get_compression_for_users(user_ids, cmp_str):
         for i in range(len(user_ids)):
             p = int(i * len(powers) / len(user_ids))
             compressions.append(1.0/(2**powers[p]))
-    elif cmp_str.startswith('s2:'): # s2:5:1-0 # 90% is 2x and 10% is 1x
+    elif cmp_str.startswith('s2:'): # s2:0.9:0-1 # 90% is 1x and 10% is 2x
         prop = float(cmp_str.split(':')[1])
         x = cmp_str.split(':')[2]
         powers = [int(a) for a in x.split('-')]
+        assert (len(powers) == 2)
         for i in range(len(user_ids)):
-            p = int((i / len(user_ids))**prop * len(powers))
+            p = int(float(i) / len(user_ids) > prop)
             compressions.append(1.0/(2**powers[p]))
     else:
         raise NotImplementedError
